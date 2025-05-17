7d009ac54f0de926f288dfe5109c0d8dc1517eb2
jisoohan <hanjisoo93@gmail.com>
2025-05-17 21:01:58 +0900

    dev initial commit

---
+++ src/test/java/com/example/Wearth/WearthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Wearth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
